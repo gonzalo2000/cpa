20dbcfae639394206a99be0e6708a02e16971dd4
Gonzalo Gallardo <gonzalo2000@hotmail.com>
2018-07-24 11:54:03 -0400

    Add validations for presence & size

--- app/models/sample.rb
+++ app/models/sample.rb
@@ -1,4 +1,16 @@
 class Sample < ApplicationRecord
   mount_uploader :instruction, InstructionUploader
   mount_uploader :submission, SubmissionUploader
+
+  validates :instruction, presence: true
+  validates :submission, presence: true
+  validates :language, presence: true
+  validates :level, presence: true
+  validates :score, presence: true
+  validates :mode, presence: true
+  validates :comment, presence: true
+
+  validates_size_of :instruction, maximum: 5.megabytes, message: "Should be less than 5MB"
+  validates_size_of :submission, maximum: 5.megabytes, message: "Should be less than 5MB"
+
 end
